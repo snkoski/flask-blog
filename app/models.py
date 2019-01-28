bdb4826dd3798f3675d6ee72468e7ca418128eb9
snkoski <shawnnkoski@gmail.com>
2019-01-28 10:54:22 -0500

    Finish follow methods in models. Add tests for password_hashing, avatars, follow_user, unfollow_user, follow_posts, and user_posts.

--- app/models.py
+++ app/models.py
@@ -5,6 +5,11 @@ from flask_login import UserMixin
 from hashlib import md5
 
 
+# followers table is an auxiliary table with no data but foreign keys so it doesn't need an associated model class
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
 # class inherits from db.Model, base class for all models from Flask-SQLAlchemy
 # add generic implementations of properties required by Flask-Login with UserMixin provided by Flask-Login
 # required properties included are is_authenticated, is_active, is_anonymous, and get_id()
@@ -22,6 +27,17 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    # self-referential relationship, user to user
+    # secondary configures the association table
+    # primaryjoin indicates the condition that links the left side entity (follower user) with the association table
+    # secondaryjoin indicates the condition that links the right side entity (followed user)
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic'
+    )
+
 
     # __repr__ method tells Python how to print objects of this class, useful for debugging
     def __repr__(self):
@@ -40,6 +56,32 @@ class User(UserMixin, db.Model):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size)
+
+    # methods to check if already following, start following, and stop following users
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    # invoke join operation on Post table, first argument is table to join with and second argument is the join condition. create temporary table that combines data from posts and followers tables according to the merge condition.
+    # filter posts by selecting ones where the follower_id is the same as the current users id
+    # sort the posts by their timestamp newest to oldest.
+    def followed_posts(self):
+        return Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id).order_by(
+                    Post.timestamp.desc())
+
+    def user_posts(self):
+        own = Post.query.filter_by(user_id=self.id)
+        return own.order_by(Post.timestamp.desc())
 
 # function required to help Flask-Login load a user, id passed to the function is a string so numeric IDs need to be converted to int
 @login.user_loader
